31c593aae0f6e54615b02fa7c7b3b26e2a1cd6d4
SE-MdShahadat <se.mdshahadat@gmail.com>
2023-03-23 11:44:42 +0600

    Initial Solution & WebApp Project

---
+++ MMWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MMWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
